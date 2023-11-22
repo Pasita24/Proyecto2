d23fd6259da31e597ac86fb8e6508b3af0f2c242
Pasita24 <benjapaz024@gmail.com>
2023-11-22 16:15:44 -0300

    Actualizacion Viaje Ciudades

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -7,21 +7,23 @@
 
 using namespace std;
 
-class DirectedGraph {
+class UndirectedGraph {
 private:
     unordered_map<string, int> cityIndexMap;
     vector<vector<int>> adjacencyMatrix;
     vector<string> cities;
 
 public:
-    DirectedGraph() = default;
+    UndirectedGraph() = default;
 
     void addEdge(const string& src, const string& dest, int weight) {
         int srcIndex = getIndex(src);
         int destIndex = getIndex(dest);
 
         adjacencyMatrix[srcIndex][destIndex] = weight;
-        cout << "Conexion agregada: " << src << " -> " << dest << " con peso: " << weight << endl;
+        adjacencyMatrix[destIndex][srcIndex] = weight; // Agregar la conexión inversa
+
+        cout << "Conexion agregada: " << src << " <-> " << dest << " con peso: " << weight << endl;
     }
 
     int getIndex(const string& city) {
@@ -29,10 +31,13 @@ public:
             int newIndex = cities.size();
             cities.push_back(city);
             cityIndexMap[city] = newIndex;
-            adjacencyMatrix.emplace_back(cities.size(), 0);
+
+            // Ajustar la matriz de adyacencia al tamaño correcto
             for (auto& row : adjacencyMatrix) {
                 row.resize(cities.size(), 0);
             }
+            adjacencyMatrix.resize(cities.size(), vector<int>(cities.size(), 0));
+
             return newIndex;
         }
         return cityIndexMap[city];
@@ -56,11 +61,19 @@ public:
         }
     }
 
-    bool hasConnection(const string& startCity, const string& endCity) {
-        int startIndex = getIndex(startCity);
-        int endIndex = getIndex(endCity);
+    vector<string> getConnectedCities(const string& city) {
+        vector<string> connectedCities;
+        int cityIndex = getIndex(city);
 
-        return adjacencyMatrix[startIndex][endIndex] != 0;
+        cout << "Ciudades conectadas a " << city << ":" << endl;
+        for (size_t i = 0; i < cities.size(); ++i) {
+            if (adjacencyMatrix[cityIndex][i] != 0) {
+                connectedCities.push_back(cities[i]);
+                cout << cities[i] << endl;
+            }
+        }
+
+        return connectedCities;
     }
 };
 
@@ -73,7 +86,7 @@ int main() {
         cout << "Archivo abierto correctamente." << endl;
     }
 
-    DirectedGraph graph;
+    UndirectedGraph graph;
 
     string line;
     getline(file, line); // Ignorar la primera línea
@@ -89,15 +102,16 @@ int main() {
 
     file.close();
 
-    // graph.printDetailedGraph(); // Imprimir lista detallada de ciudades y conexiones
+    graph.printDetailedGraph(); // Imprimir lista detallada de ciudades y conexiones
 
-    // Verificar si hay conexión entre dos ciudades
-    string startCity = "Silverstone City";
-    string endCity = "Valley City";
-    if (graph.hasConnection(startCity, endCity)) {
-        cout << "Hay conexion entre " << startCity << " y " << endCity << endl;
-    } else {
-        cout << "No hay conexion entre " << startCity << " y " << endCity << endl;
+    // Obtener las ciudades conectadas a "Capital City"
+    string startCity = "Capital City";
+    vector<string> connectedCities = graph.getConnectedCities(startCity);
+
+    // Mostrar las ciudades conectadas en un menú
+    cout << "Ciudades a las que puedes viajar desde " << startCity << ":" << endl;
+    for (size_t i = 0; i < connectedCities.size(); ++i) {
+        cout << i + 1 << ". " << connectedCities[i] << endl;
     }
 
     return 0;
