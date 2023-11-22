252532654b39b1aa9d517c9803515083709020aa
Pasita24 <benjapaz024@gmail.com>
2023-11-22 17:37:20 -0300

    Actualizacion
    Se ordeno el codigo para dividirlo en partes a traves del menu , quedan los ultimos detalles de la ciudad , sus movimiento y informacion

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -7,6 +7,13 @@
 
 using namespace std;
 
+struct Guardian {
+    string name;
+    int powerLevel;
+    string master;
+    string city;
+    vector<string> apprentices;
+};
 class UndirectedGraph {
 private:
     unordered_map<string, int> cityIndexMap;
@@ -26,6 +33,18 @@ public:
         cout << "Conexion agregada: " << src << " <-> " << dest << " con peso: " << weight << endl;
     }
 
+    size_t getNumberOfCities() const {
+        return cities.size();
+    }
+
+    // Obtener el nombre de una ciudad por su índice
+    string getCityName(size_t index) const {
+        if (index < cities.size()) {
+            return cities[index];
+        }
+        return "";
+    }
+
     int getIndex(const string& city) {
         if (cityIndexMap.find(city) == cityIndexMap.end()) {
             int newIndex = cities.size();
@@ -42,8 +61,7 @@ public:
         }
         return cityIndexMap[city];
     }
-
-    void printDetailedGraph() {
+      void printDetailedGraph() {
         cout << "Ciudades y sus conexiones:" << endl;
         for (size_t i = 0; i < cities.size(); ++i) {
             cout << cities[i] << " conectada a: ";
@@ -59,6 +77,22 @@ public:
             }
             cout << endl;
         }
+    }
+    void printCityConnections(const string& city) {
+        int cityIndex = getIndex(city);
+
+        cout << city << " conectada a: ";
+        bool hasConnection = false;
+        for (size_t i = 0; i < cities.size(); ++i) {
+            if (adjacencyMatrix[cityIndex][i] != 0) {
+                cout << cities[i] << " (peso: " << adjacencyMatrix[cityIndex][i] << ") ";
+                hasConnection = true;
+            }
+        }
+        if (!hasConnection) {
+            cout << "Ninguna conexion";
+        }
+        cout << endl;
     }
 
     vector<string> getConnectedCities(const string& city) {
@@ -78,41 +112,143 @@ public:
 };
 
 int main() {
-    ifstream file("Ciudades.txt");
-    if (!file.is_open()) {
-        cout << "No se pudo abrir el archivo." << endl;
+    ifstream cityFile("Ciudades.txt");
+    if (!cityFile.is_open()) {
+        cout << "No se pudo abrir el archivo de ciudades." << endl;
         return 1;
     } else {
-        cout << "Archivo abierto correctamente." << endl;
+        cout << "Archivo de ciudades abierto correctamente." << endl;
     }
 
     UndirectedGraph graph;
 
-    string line;
-    getline(file, line); // Ignorar la primera línea
+    string cityLine;
+    getline(cityFile, cityLine); // Ignorar la primera línea
 
-    while (getline(file, line)) {
-        istringstream iss(line);
+    while (getline(cityFile, cityLine)) 
+    {
+        istringstream iss(cityLine);
         string city, connection;
         if (getline(iss, city, ',') && getline(iss, connection)) {
-            int weight = 1; // Peso por defecto
-            graph.addEdge(city, connection, weight); // Agregar la conexión al grafo
+                int weight = 1; // Peso por defecto
+                graph.addEdge(city, connection, weight); // Agregar la conexión al grafo
+            }
+    }
+
+    cityFile.close();
+        //Podemos hacer un menu que ingrese que desea buscar con sus conexiones
+    /*graph.printDetailedGraph(); 
+    graph.printCityConnections("Capital City"); // Imprimir lista detallada de ciudades y conexiones*/
+
+    ifstream guardianFile("Guardianes.txt");
+    if (!guardianFile.is_open()) {
+        cout << "No se pudo abrir el archivo de guardianes." << endl;
+        return 1;
+    } else {
+        cout << "Archivo de guardianes abierto correctamente." << endl;
+    }
+
+    vector<Guardian> guardians; // Vector para almacenar los guardianes
+
+    string guardianLine;
+    getline(guardianFile, guardianLine); // Ignorar la primera línea
+
+    while (getline(guardianFile, guardianLine)) {
+        istringstream iss(guardianLine);
+        string name, powerLevelStr, master, city;
+        if (getline(iss, name, ',') && getline(iss, powerLevelStr, ',') && 
+            getline(iss, master, ',') && getline(iss, city, ',')) {
+            int powerLevel = stoi(powerLevelStr);
+
+            Guardian guardian;
+            guardian.name = name;
+            guardian.powerLevel = powerLevel;
+            guardian.master = master;
+            guardian.city = city;
+
+            guardians.push_back(guardian);
         }
     }
 
-    file.close();
+    guardianFile.close();
 
-    graph.printDetailedGraph(); // Imprimir lista detallada de ciudades y conexiones
+     bool exitGame = false;
 
-    // Obtener las ciudades conectadas a "Capital City"
-    string startCity = "Capital City";
-    vector<string> connectedCities = graph.getConnectedCities(startCity);
+    while (!exitGame) {
+        cout << "Bienvenido al Juego de Guardianes" << endl;
+        cout << "Menu:" << endl;
+        cout << "1. Ver la lista de candidatos" << endl;
+        cout << "2. Ver al guardian" << endl;
+        cout << "3. Conocer el reino" << endl;
+        cout << "4. Presenciar una Batalla" << endl;
+        cout << "5. Salir" << endl;
 
-    // Mostrar las ciudades conectadas en un menú
-    cout << "Ciudades a las que puedes viajar desde " << startCity << ":" << endl;
-    for (size_t i = 0; i < connectedCities.size(); ++i) {
-        cout << i + 1 << ". " << connectedCities[i] << endl;
+        int choice;
+        cout << "Ingresa tu eleccion: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                // Ver la lista de candidatos (Guardianes)
+                cout << "Guardianes:" << endl;
+                for (const auto& guardian : guardians) {
+                    cout << "Nombre: " << guardian.name << endl;
+                    cout << "Nivel de Poder: " << guardian.powerLevel << endl;
+                    cout << "Maestro: " << guardian.master << endl;
+                    cout << "Ciudad: " << guardian.city << endl;
+                    cout << endl;
+                }
+                break;
+            case 2:
+                // Ver al guardián (implementación pendiente)
+                cout << "Funcionalidad aun no implementada." << endl;
+                break;
+            case 3:
+            // Conocer el reino
+            cout << "1. Ver la lista de ciudades y sus conexiones" << endl;
+            cout << "2. Conocer las conexiones de una ciudad especifica" << endl;
+
+            int kingdomChoice;
+            cout << "Ingresa tu eleccion: ";
+            cin >> kingdomChoice;
+
+            if (kingdomChoice == 1) {
+                graph.printDetailedGraph();
+            } else if (kingdomChoice == 2) {
+                // Mostrar un menú de ciudades disponibles
+                cout << "Ciudades disponibles:" << endl;
+                for (size_t i = 0; i < graph.getNumberOfCities(); ++i) {
+                    cout << i + 1 << ". " << graph.getCityName(i) << endl;
+                }
+
+                int cityNumber;
+                cout << "Ingresa el numero de la ciudad para conocer sus conexiones: ";
+                cin >> cityNumber;
+
+                if (cityNumber >= 1 && cityNumber <= graph.getNumberOfCities()) {
+                    string cityToCheck = graph.getCityName(cityNumber - 1);
+                    graph.printCityConnections(cityToCheck);
+                } else {
+                    cout << "Numero de ciudad invalido." << endl;
+                }
+            } else {
+                cout << "Opcion invalida." << endl;
+            }
+            break;
+            case 4:
+                // Presenciar una Batalla (implementación pendiente)
+                cout << "Funcionalidad aun no implementada." << endl;
+                break;
+            case 5:
+                exitGame = true;
+                break;
+            default:
+                cout << "Opcion invalida. Por favor, elige una opcion del menu." << endl;
+                break;
+        }
     }
+
+    cout << "Gracias por jugar. Hasta luego." << endl;
 
     return 0;
 }
